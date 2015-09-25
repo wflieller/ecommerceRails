90f52e1f240b7b2793ed368618e656512a9d6c2e
Walker Flieller <wflieller@gmail.com>
2015-09-24 20:46:37 -0500

    User sign in/registrations created

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -1,2 +1,24 @@
 class SubscribersController < ApplicationController
+
+	before_filter :authenticate_user!
+
+	def new
+	end
+
+	def update
+		token = params[:stripeToken]
+
+		customer = Stripe::Customer.create(
+			card: token,
+			plan: ,
+			email: current_user.email
+			)
+		current_user.subscribed = true
+		current_user.stripeid = customer.id
+		current_user.save
+
+		redirect_to dashboard_path
+
+	end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150924183250) do
+ActiveRecord::Schema.define(version: 20150924214403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "invoices", force: :cascade do |t|
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -27,10 +30,17 @@ ActiveRecord::Schema.define(version: 20150924183250) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.boolean  "subscribed"
     t.string   "stripeid"
+    t.string   "username",                            null: false
+    t.string   "firstname",                           null: false
+    t.string   "lastname",                            null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
